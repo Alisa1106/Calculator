cf1823fcb85468edf1d8a8876b676e7d8a4cd295
Alisa Harodnik <alisa.solovchuk@mail.ru>
2021-07-18 19:53:03 +0300

    add circleCi

--- src/test/java/tests/AdditionTest.java
+++ src/test/java/tests/AdditionTest.java
@@ -1,6 +1,8 @@
 package tests;
 
 import org.testng.Assert;
+import org.testng.annotations.Optional;
+import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
 public class AdditionTest extends BaseTest implements ITestData {
@@ -14,5 +16,11 @@ public class AdditionTest extends BaseTest implements ITestData {
             description = "Add two numbers via dataProvider", priority = 1)
     public void addNumbersViaDataProviderTest(double firstNumber, double secondNumber) {
         Assert.assertEquals(calculator.add(firstNumber, secondNumber), firstNumber + secondNumber);
+    }
+
+   @Parameters({"firstNumber", "secondNumber"})
+    @Test
+    public void add(@Optional("1") double p1, @Optional("1") double p2) {
+        Assert.assertEquals(calculator.add(p1,p2), p1 + p2);
     }
 }
